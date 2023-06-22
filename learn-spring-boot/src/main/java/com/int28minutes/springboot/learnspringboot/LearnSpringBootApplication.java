c14f1331e03ad07b7a427d5e50031566d603bf93
tein1142 <tein1142@hotmail.com>
2023-06-22 22:18:36 +0700

    [ADD] Initial dev branch

--- learn-spring-boot/src/main/java/com/int28minutes/springboot/learnspringboot/LearnSpringBootApplication.java
+++ learn-spring-boot/src/main/java/com/int28minutes/springboot/learnspringboot/LearnSpringBootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearnSpringBootApplication {
-
+	//Main
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSpringBootApplication.class, args);
 	}
